cc81873f56ef190ecd5f3eed91ddb4c81c9e7cba
currandj <currandj@LCBLAB2110-19.uccob.uc.edu>
2020-02-25 09:18:17 -0500

    Last Commit for the day

--- In_Class_2_25_2020/Src/Main.py
+++ In_Class_2_25_2020/Src/Main.py
@@ -8,6 +8,13 @@ from image_functions import *
 # open an image file. The default path is where this python module is
 im = Image.open("SiriusAndViolet.jpg")
 print(im.width, im.height, im.mode, im.format)  # Display some info about the image
-
+'''
 my_image = load_image("XSiriusAndViolet.jpg")
 my_image.show(my_image)
+'''
+#Make use of crop_image
+#1. Load Image, 2. Call crop Image and save what it returns, 3. Call show to display results
+
+im = Image.open("SiriusAndViolet.jpg")
+im_cropped = crop_image(im, (200,300,400,500))
+im_cropped.show()
--- In_Class_2_25_2020/Src/image_functions.py
+++ In_Class_2_25_2020/Src/image_functions.py
@@ -16,3 +16,18 @@ def load_image( filename ) :
     except:
         print("Load_Image(): Unable to open " + filename)
         return None #None is a keyword that represents a null pointer
+    
+def save_image( imageObject, outfilename ) :
+    """
+    Save an image to disk
+    :param imageObject: The Image to save
+    :param outfilename: The target file
+    """
+    try:
+        imageObject.save( outfilename )
+    except:
+        print("save_image(): Unable to save " + outfilename)
+
+def crop_image(imageObject, cropRegion):        
+    im_c = imageObject.crop(cropRegion) # (left, top, right, bottom) it's a tuple!
+    return im_c
